6518b7aefbb00cc4a5e205e355d0b881194b49f8
Guilherme Haschel <ghaschel@gmail.com>
2026-01-05 16:19:15 -0300

    feat: added option to hide and show commit sections from the changelog

--- .versionrc.js
+++ .versionrc.js
@@ -6,4 +6,17 @@ module.exports = {
     postcommit: "node scripts/split-changelog.mjs",
     posttag: "node scripts/custom-tag-message.mjs",
   },
+  types: [
+    { type: "feat", section: "✨ Features" },
+    { type: "fix", section: "🐛 Bug Fixes" },
+    { type: "perf", section: "⚡ Performance" },
+    { type: "refactor", section: "♻️ Refactors" },
+    { type: "docs", section: "📚 Documentation", hidden: true },
+    { type: "test", section: "🧪 Tests", hidden: true },
+    { type: "build", section: "🔧 Build", hidden: true },
+    { type: "ci", section: "🔁 CI", hidden: true },
+    { type: "chore", section: "🧹 Chores", hidden: true },
+    { type: "revert", section: "⏪ Reverts", hidden: true },
+    { type: "style", section: "💄 Styles", hidden: true },
+  ],
 };
